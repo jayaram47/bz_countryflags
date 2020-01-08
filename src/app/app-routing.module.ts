47a3c966c42068e061e6ce78c9752152c9de981c
jayaram47 <jayaram47puli@gmail.com>
2020-01-08 12:00:51 +0530

    intermediate

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ColourComponent } from './colour/colour.component';
+import { UserComponent } from './user/user.component';
+import { HomeComponent } from './home/home.component';
+import { CountryComponent } from './country/country.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+{path: 'home',component:HomeComponent},
+{path:'colors',component:ColourComponent},
+{path:'user',component:UserComponent},
+{path:'countries',component:CountryComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
