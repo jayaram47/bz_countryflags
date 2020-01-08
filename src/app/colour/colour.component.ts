c5e372a9b9ecfe85265347f0cdd04269c7cb0fb6
jayaram47 <jayaram47puli@gmail.com>
2020-01-08 13:45:29 +0530

    final_output

--- src/app/colour/colour.component.ts
+++ src/app/colour/colour.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-
+import { DataService } from '../data.service';
 @Component({
   selector: 'app-colour',
   templateUrl: './colour.component.html',
   styleUrls: ['./colour.component.css']
 })
 export class ColourComponent implements OnInit {
-
-  constructor() { }
+  colour;
+  constructor(private data: DataService) { }
 
   ngOnInit() {
+    this.data.getColours().subscribe(d => {this.colour = d;}
+      )
   }
 
 }
+
